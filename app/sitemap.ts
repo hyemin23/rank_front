86cf9841c523ba70438208f146948897de34a2a8
박혜민 <xion@baghyemin-ui-MacBookAir.local>
2024-02-28 11:19:17 +0900

    fix

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,19 +4,24 @@ const domain = "https://www.luxuryranking.net";
 export default async function sitemap() {
   const data = await getCardData("watch");
 
-  const cate = ["watch"]?.map((cdata) => ({
+  const cate = ["watch"].map((cdata) => ({
     url: `${domain}/${cdata}`,
     lastModified: new Date(),
     changeFrequency: "weekly",
     priority: 1,
   }));
 
-  const routes = data?.map((dedata: any) => ({
-    url: `${domain}/watch/${dedata.name}`,
-    lastModified: new Date(),
-    changeFrequency: "weekly",
-    priority: 1,
-  }));
+  let routes: any = [];
+  if (data) {
+    routes = data?.map((dedata: any) => ({
+      url: `${domain}/watch/${dedata.name}`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    }));
+
+    return routes;
+  }
 
   return [...cate, ...routes];
 }
