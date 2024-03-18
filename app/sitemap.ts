d8cfabc41f54db2a330e5f99b5c5dacbcd65ef0d
박혜민 <xion@baghyemin-ui-MacBookAir.local>
2024-03-19 01:18:38 +0900

    sitemap xml

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,27 +1,27 @@
-import { getCardData } from "./_lib/getCardData";
+// import { getCardData } from "./_lib/getCardData";
 
-const domain = "https://www.luxuryranking.net";
-export default async function sitemap() {
-  const data = await getCardData("watch");
+// const domain = "https://www.luxuryranking.net";
+// export default async function sitemap() {
+//   const data = await getCardData("watch");
 
-  const cate = ["watch"].map((cdata) => ({
-    url: `${domain}/${cdata}`,
-    lastModified: new Date(),
-    changeFrequency: "weekly",
-    priority: 1,
-  }));
+//   const cate = ["watch"].map((cdata) => ({
+//     url: `${domain}/${cdata}`,
+//     lastModified: new Date(),
+//     changeFrequency: "weekly",
+//     priority: 1,
+//   }));
 
-  let routes: any = [];
-  if (data) {
-    routes = data?.map((dedata: any) => ({
-      url: `${domain}/watch/${dedata.name}`,
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 1,
-    }));
+//   let routes: any = [];
+//   if (data) {
+//     routes = data?.map((dedata: any) => ({
+//       url: `${domain}/watch/${dedata.name}`,
+//       lastModified: new Date(),
+//       changeFrequency: "weekly",
+//       priority: 1,
+//     }));
 
-    return routes;
-  }
+//     return routes;
+//   }
 
-  return [...cate, ...routes];
-}
+//   return [...cate, ...routes];
+// }
